78e0abc3abdcc0dd0bd8914d8857ca95cd102237
Jordan White <jordanrw@vt.edu>
2015-08-01 12:45:56 -0700

    initial commit

---
+++ animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  animation
+//
+//  Created by Jordan White on 8/1/15.
+//  Copyright (c) 2015 Two Beards and Fro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ animation/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  animation
+//
+//  Created by Jordan White on 8/1/15.
+//  Copyright (c) 2015 Two Beards and Fro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - animations
+- (IBAction)animateButton:(UIButton *)sender {
+    NSLog(@"hi");
+}
+
+
+
+@end
