39786c6bac0032b57f674962d0346c86fb25b5b9
Jordan White <jordanrw@vt.edu>
2015-08-01 13:11:56 -0700

    UIView "login field" set up programmatically

--- animation/ViewController.m
+++ animation/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,12 +33,42 @@
 }
 
 - (UIView*)createUIView {
-    //UIView *view = [UIView alloc]initWithFrame:CGRectMake(self.view.center), CGRectMake(self.view.center), 50, 50;
-    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(120, 120, 50.0, 50.0)];
-    view.backgroundColor = [UIColor redColor];
+
+    //CGPoint cp = self.view.center;
+    double width = 275.0;
+    double height = 325.0;
+    
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake((self.view.bounds.size.width)/2 - width/2 ,(self.view.bounds.size.height)/2 - height/2 , width, height)];
+    
+    view.backgroundColor = [UIColor blackColor];
+    
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(25, 25, width - 50, 25)];
+    label.text = @"FBI Database";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor whiteColor];
+    [view addSubview:label];
+    
+    
+    UITextField *nameField = [[UITextField alloc]initWithFrame:CGRectMake(25, 100, width - 50, 30)];
+    nameField.delegate = self;
+    nameField.backgroundColor = [UIColor whiteColor];
+    nameField.placeholder = @"Your Name";
+    [view addSubview:nameField];
+    
+    UITextField *passwordField = [[UITextField alloc]initWithFrame:CGRectMake(25, 150, width - 50, 30)];
+    passwordField.delegate = self;
+    passwordField.backgroundColor = [UIColor whiteColor];
+    passwordField.placeholder = @"password";
+    [view addSubview:passwordField];
+    
     
     return view;
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    
+    return YES;
+}
 
 @end
